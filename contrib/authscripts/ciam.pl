4d9e8078661230ce16bf579e6d2a9fe3fa198b14
hvogler <hartmut.vogler@t-systems.com>
2019-02-05 15:03:29 +0100

    extend ciam.pl to allow ciam/[tCID], ciam/[EMEA-Account] and ciam/[WIWID] as usernames to mod_auth_ae domain ciam

--- contrib/authscripts/ciam.pl
+++ contrib/authscripts/ciam.pl
@@ -1,4 +1,5 @@
 #!/usr/bin/perl
+use Net::LDAP;
 use strict;
 use Data::Dumper;
 use HTML::Parser;
@@ -16,6 +17,98 @@ my $ua=new LWP::UserAgent();
 
 my $ciamurl=$ARGV[0];
 my $username=$ARGV[1];
+
+if (!($username=~m/^.+\@.+$/)){
+   printf STDERR ("INFO: Remapping needed\n");
+   my $LDAPSERVER=$ARGV[2];
+   my $LDAPSEARCHUSER=$ARGV[3];
+   my $LDAPSEARCHPASS=$ARGV[4];
+   my $LDAPBASE="ou=Person,o=DTAG";
+   my $ldaph;
+   my $msg;
+   
+   #
+   # uid DN suchen
+   #
+   $ldaph=new Net::LDAP($LDAPSERVER);
+   if (!defined($ldaph) || ($ldaph) == -1){
+      printf STDERR ("ERROR: Connection to %s Failed!\n",$LDAPSERVER);
+      exit(1);
+   }
+   #
+   # anonym anmelden
+   #
+   #printf STDERR ("LDAPSEARCHUSER='$LDAPSEARCHUSER' ".
+   #               "LDAPSEARCHPASS='$LDAPSEARCHUSER'\n");
+   if ($LDAPSEARCHUSER){
+      $msg=$ldaph->bind($LDAPSEARCHUSER,password=>$LDAPSEARCHPASS);
+   }
+   else{
+      $msg=$ldaph->bind();
+   }
+
+   if ($msg->code){
+       printf STDERR ("ERROR: ldaph->bind code=%d msg=%s\n",
+                      $msg->code,$msg->error);
+       exit(1);
+   }
+   my @attrs=("mail");
+   my %flt=(
+      base=>$LDAPBASE,
+      scope=>"sub",
+      attrs=>\@attrs
+   );
+   if ($username=~m/^a\d{3,10}$/i){
+      # Username is a tCID and needs to be translated
+      $username=~s/^a//;
+      $flt{filter}="(tcid=$username)";
+   }
+   elsif ($username=~m/^\d{3,10}$/){
+      # Username is a tCID and needs to be translated
+      $flt{filter}="(tcid=$username)";
+   }
+   elsif ($username=~m/^[a-z0-9]{4,8}$/){
+      # Username is eine WIWID
+      $flt{filter}="(twiw-uid=$username)";
+   }
+   else{
+      exit(1);
+   }
+   $flt{filter}="(&(tisActive=true)(tisPrimary=true)$flt{filter})";
+
+   printf STDERR ("INFO: remap filter=%s\n",$flt{filter});
+   my $msg=$ldaph->search(%flt);
+   if ($msg->code){
+       printf STDERR ("ERROR: ldaph->bind code=%d msg=%s\n",
+                      $msg->code,$msg->error);
+       exit(1);
+   }
+   my $remapped=0;
+   LDAPFILTER: foreach my $entry ($msg->entries()){
+      foreach my $attr ($entry->attributes) {
+         if ($attr eq "mail"){
+            my @val=$entry->get_value($attr);
+            @val=grep(!/^\s*$/,@val);
+            if ($val[0] ne ""){
+               $username=$val[0];
+               $remapped++;
+               last LDAPFILTER;
+            }
+         }
+      }
+   }
+   $msg=$ldaph->unbind();
+   if (!$remapped){
+      printf STDERR ("ERROR: user remap failed\n");
+      exit(1);
+   }
+   printf STDERR ("INFO: User remaped to $username\n");
+}
+
+
+
+
+
 my $password=<STDIN>;
 $password=~s/\s$//;
 #printf STDERR ("using CIAM URL='%s'\nusername='%s'\n",$ciamurl,$username);
